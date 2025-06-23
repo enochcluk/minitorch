7f20ebcf0ae98e1add316cf8c2f30363a2d0f301
Enoch Luk <66643403+enochcluk@users.noreply.github.com>
2025-06-22 21:48:15 -0700

    downgraded numpy, downgraded altair, somehow reinstalling pytorch (also a downgrade) in order for streamlit to work

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
-from setuptools import setup
+# from setuptools import setup
 
-setup(py_modules=[])
+# setup(py_modules=[])
+from setuptools import setup, find_packages  # <- this line is critical!
+
+setup(
+    name="minitorch",
+    version="0.0.1",
+    packages=find_packages(),
+    include_package_data=True,
+)
